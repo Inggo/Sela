1f6ec054d3170c5c491d9f1aa22c85c0149f69a4
Inggo <inggo.espinosa@gmail.com>
2018-01-21 23:36:01 +0800

    Add caption

--- gallery.php
+++ gallery.php
@@ -4,11 +4,13 @@ $i = 0;
 foreach ($attachments as $id => $attachment) {
     $thumb = wp_get_attachment_image_src($id, 'thumbnail');
     $image = wp_get_attachment_image_src($id, 'large');
+    $caption = wp_get_attachment_caption($id);
 ?>
     <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
         <a href="<?= $image[0] ?>" itemprop="contentUrl" data-size="<?= $image[1]; ?>x<?= $image[2]; ?>">
             <img src="<?= $thumb[0] ?>" itemprop="thumbnail" alt="">
         </a>
+        <figcaption itemprop="caption description"><?= $caption; ?></figcaption>
     </figure>
 <?php
 }
