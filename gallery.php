7bfcac79b5a5223ad38f241648f7831a2da24f8c
Inggo <inggo.espinosa@gmail.com>
2018-01-21 23:27:06 +0800

    Fix gallery markup

--- gallery.php
+++ gallery.php
@@ -4,13 +4,10 @@ $i = 0;
 foreach ($attachments as $id => $attachment) {
     $thumb = wp_get_attachment_image_src($id, 'thumbnail');
     $image = wp_get_attachment_image_src($id, 'large');
-
-    var_dump($thumb);
-    var_dump($image);
 ?>
     <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-        <a href="<?= $image['url'] ?>" itemprop="contentUrl" data-size="<?= $image['width']; ?>x<?= $image['height']; ?>">
-            <img src="<?= $thumb['url'] ?>" itemprop="thumbnail" alt="">
+        <a href="<?= $image[0] ?>" itemprop="contentUrl" data-size="<?= $image[1]; ?>x<?= $image[2]; ?>">
+            <img src="<?= $thumb[0] ?>" itemprop="thumbnail" alt="">
         </a>
     </figure>
 <?php
